ab205edf909f7493ad1f7cf30af70c22a2803090
gourav121992 <gouravghosh6@gmail.com>
2021-05-29 14:24:11 +0530

    thirst commit

---
+++ question-own-3.js
@@ -0,0 +1,41 @@
+// OLD & POOR SYNTAX 
+// function Book (title,pages,isbn)
+//     {
+//         this.title =title;
+//         this.pages = pages;
+//         this.isbn = isbn;
+
+//     };
+//     Book.prototype.printTitle = function(){
+//         console.log(this.title);
+//     }
+
+// SYNTAX WITH A CLASS IMPLEMENTATION 
+
+ class Book{
+     constructor (title,pages,isbn)
+         {
+             this.title = title;
+             this.pages = pages;
+             this.isbn = isbn;
+         }
+         PrintIsbn(){
+             console.log(this.isbn);
+         };
+ };
+
+ let book = new Book('title','pages','isbn');
+ console.log(book.title,book.pages,book.isbn);
+
+ class BOOK{
+     constructor(books)
+        {
+            this.books =books;
+            
+        }
+          
+ };
+
+ let MyBooks = new BOOK('Harrypotter');
+ console.log(MyBooks);
+
